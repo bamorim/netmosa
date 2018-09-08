8acda915fb67ae8c1cb8ecc85187445ca6db2559
Bernardo Amorim <contato@bamorim.com>
2018-09-08 15:37:15 -0300

    feat: added basic react setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import {Edge} from './Model';
 import {randomWalkModel} from './Models';
 import {Simulation} from './Simulator';
 
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Main } from './components/Main';
+
 const k = 2;
 const n = 100;
 
@@ -16,3 +21,8 @@ from(simulation)
     .pipe(take(n))
     .subscribe((x: Edge) => console.log('' + x[0] + ' -> ' + x[1]));
 console.log('}');
+
+ReactDOM.render(
+    <Main compiler="TypeScript" framework="React" />,
+    document.getElementById('example'),
+);
