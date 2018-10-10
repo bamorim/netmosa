1438eff425a52549fd53f813fe5c625fe0aebf3d
Bernardo Amorim <contato@bamorim.com>
2018-10-10 02:58:37 -0300

    feat: more generic barabasi albert

--- examples/barabasi_albert.lua
+++ examples/barabasi_albert.lua
@@ -1,12 +1,25 @@
--- Start with two connected vertices
-connectVertices(addVertex(), addVertex())
+m0 = 2
+m = 2
+initials = {}
+
+for i=1,m0 do
+  initials[i] = addVertex()
+  for j=1,(i-1) do
+    connectVertices(j, i)
+  end
+end
 
 while true do
-  -- Get the edge vertices and put it on a table
-  vertices = {getEdge(math.random(getEdgeCount()))}
+  -- Add a new vertex
+  newVertex = addVertex()
 
-  -- Add a new vertex connected to a random vertice from the edge
-  connectVertices(addVertex(), vertices[math.random(1,2)])
+  for i=1,m do
+    -- Get the edge vertices and put it on a table
+    vertices = {getEdge(math.random(getEdgeCount()))}
+
+    -- Connect the new vertex to a random vertice from the edge
+    connectVertices(newVertex, vertices[math.random(1,2)])
+  end
 
   -- Give a chance to render
   coroutine.yield()
