82de8f19b524b5ddd2c7f822571d4243afb1820c
Bernardo Amorim <contato@bamorim.com>
2019-02-08 07:02:15 -0500

    feat: scientific notation

--- src/components/DistributionChart/Chart.tsx
+++ src/components/DistributionChart/Chart.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react'
-import { LineChart, CartesianGrid, XAxis, YAxis, Legend, Line, Tooltip } from 'recharts'
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Legend,
+  Line,
+  Tooltip
+} from 'recharts'
 import { Datum } from './types'
+import { maxBy, minBy, reduce, range } from 'ramda'
+
+interface SNProps {
+  number: number
+}
+
+const trimmedExponential = (number: number) => {
+  const [mantissa, exponent] = number.toExponential().split('e')
+  return `${mantissa.substring(0, 4)}e${exponent}`
+}
+
+const ScientificNumber = ({ number }: SNProps) => {
+  const [mantissa, exponent] = trimmedExponential(number).split('e')
+  const normalExponent = exponent.substring(0,1) == "+" ? exponent.substring(1) : exponent
+  return <span>{mantissa} 10<sup>{normalExponent}</sup></span>
+}
 
 interface Props {
   xLog: boolean
@@ -9,15 +33,21 @@ interface Props {
   name: string
 }
 
+const config = (log: boolean, values: number[]) => {
+  const normalize = log ? (v: number) => Math.pow(10, v) : (v: number) => v
+
+  const maxVal = reduce(maxBy((x: number) => x), -Infinity, values)
+  const minVal = reduce(minBy((x: number) => x), +Infinity, values)
+  const baseTicks = range(Math.ceil(minVal), Math.floor(maxVal) + 1)
+  const ticks = log ? baseTicks : baseTicks.filter(x => x % 5 == 0)
+
+  return { ticks, normalize }
+}
+
 class Chart extends React.PureComponent<Props> {
   public render() {
-    const xLogFormatter = (x: number) => Math.round(Math.pow(10,x))
-    const yLogFormatter = (y: number) =>
-      `${Math.round(Math.pow(10, y) * 10000) / 10000}`
-    const yLinFormatter = (y: number) => `${Math.round(y * 10000) / 10000}`
-
-    const xFormatter = this.props.xLog ? xLogFormatter : (x: number) => x
-    const yFormatter = this.props.yLog ? yLogFormatter : yLinFormatter
+    const xConfig = config(this.props.xLog, this.props.data.map(({ x }) => x))
+    const yConfig = config(this.props.yLog, this.props.data.map(({ y }) => y))
 
     return (
       <LineChart
@@ -31,17 +61,26 @@ class Chart extends React.PureComponent<Props> {
           type="number"
           dataKey="x"
           domain={['dataMin', 'dataMax']}
-          tickFormatter={xFormatter}
+          ticks={xConfig.ticks}
+          tickFormatter={(number) => trimmedExponential(xConfig.normalize(number))}
         />
-        <YAxis domain={['dataMin', 'dataMax']} tickFormatter={yFormatter} />
+        <YAxis
+          domain={['dataMin', 'dataMax']}
+          ticks={yConfig.ticks}
+          tickFormatter={(number) => trimmedExponential(yConfig.normalize(number))}
+        />
         <Legend verticalAlign="top" height={36} />
-        <Tooltip/>
+        <Tooltip
+          formatter={(value: number) => <ScientificNumber number={yConfig.normalize(value)}/>}
+          labelFormatter={(value: number) => <span>
+            {this.props.name}: <ScientificNumber number={xConfig.normalize(value)}/>
+            </span>}
+        />
         <Line
-          type="monotone"
           dataKey="y"
           stroke="#8884d8"
           isAnimationActive={false}
-          name={this.props.name}
+          name="P"
         />
       </LineChart>
     )
