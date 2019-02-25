4e8c55330b5b82cd33505f1a1446aa9933f473ad
Bernardo Amorim <contato@bamorim.com>
2019-02-24 21:13:05 -0300

    fix: distance to root distribution

--- src/scenes/Simulation/scenes/Statistics/services/DistanceToRootDistributionCollector.ts
+++ src/scenes/Simulation/scenes/Statistics/services/DistanceToRootDistributionCollector.ts
@@ -1,6 +1,6 @@
 import { ReplaySubject, Subject, Subscription } from 'rxjs'
 
-import { ReadGraph, Change } from 'graph'
+import { ReadGraph, Change, VertexId } from 'graph'
 
 const changeDist = (dist: number[], idx: number, amt: number) => {
   dist[idx] = dist[idx] || 0
@@ -10,7 +10,7 @@ const changeDist = (dist: number[], idx: number, amt: number) => {
 class DistanceToRootDistributionCollector {
   private graph: ReadGraph
   private distribution: number[]
-  private distance: number[]
+  private distances: Map<VertexId, number>
   private subscription: Subscription
   private distributionSubject: Subject<number[]> = new ReplaySubject(1)
 
@@ -20,7 +20,7 @@ class DistanceToRootDistributionCollector {
     this.graph = graph
     this.subscription = this.graph.change$.subscribe(this.onGraphEvent)
     this.distribution = []
-    this.distance = []
+    this.distances = new Map()
   }
 
   public destroy() {
@@ -31,30 +31,34 @@ class DistanceToRootDistributionCollector {
     switch (change.type) {
       case 'AddedVertex':
         if (change.id === 0) {
-          this.distance[change.id] = 0
+          this.distances.set(change.id, 0)
           changeDist(this.distribution, 0, +1)
-        } else {
-          this.distance[change.id] = -1
         }
         break
       case 'AddedEdge':
         const [v1, v2] = this.graph.edges[change.id]
-        const d1 = this.distance[v1]
-        const d2 = this.distance[v2]
-        if (d1 === -1 && d2 > 0) {
-          this.distance[v1] = d2 + 1
-          changeDist(this.distribution, d2 + 1, +1)
-        } else if (d1 >= 0 && d2 === -1) {
-          this.distance[v2] = d1 + 1
+        const d1 = this.distances.get(v1)
+        const d2 = this.distances.get(v2)
+        if (d1 === undefined && d2 === undefined) {
+          // no change, nothing to do
+        } else if (d1 !== undefined && d2 === undefined) {
+          this.distances.set(v2, d1 + 1)
           changeDist(this.distribution, d1 + 1, +1)
-        } else if (d1 >= 0 && d2 > d1) {
-          this.distance[v2] = d1 + 1
-          changeDist(this.distribution, d2, -1)
-          changeDist(this.distribution, d1 + 1, +1)
-        } else if (d2 >= 0 && d1 > d2) {
-          this.distance[v1] = d2 + 1
-          changeDist(this.distribution, d1, -1)
+        } else if (d2 !== undefined && d1 === undefined) {
+          this.distances.set(v1, d2 + 1)
           changeDist(this.distribution, d2 + 1, +1)
+        } else if (d1 !== undefined && d2 !== undefined) {
+          if (d2 > d1) {
+            // Decrease v2 distance
+            this.distances.set(v2, d1 + 1)
+            changeDist(this.distribution, d2, -1)
+            changeDist(this.distribution, d1 + 1, +1)
+          } else if (d1 > d2) {
+            // Decrease v1 distance
+            this.distances.set(v1, d2 + 1)
+            changeDist(this.distribution, d1, -1)
+            changeDist(this.distribution, d2 + 1, +1)
+          }
         }
         break
       default:
